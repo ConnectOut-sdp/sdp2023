5dc0b99d9ccc03f1250ffd429b88136bb6134f76
David Kalajdzic <david.kalajdzic@icloud.com>
2023-02-27 13:28:23 +0100

    GreetingActivity with tests (sample app)

---
+++ app/src/androidTest/java/com/sdpteam/connectout/MainActivityTest.java
@@ -0,0 +1,52 @@
+package com.sdpteam.connectout;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import org.hamcrest.Matcher;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.view.View;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.matcher.IntentMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> testRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Before
+    public void setup() {
+        Intents.init();
+    }
+
+    @After
+    public void cleanup() {
+        Intents.release();
+    }
+
+    @Test
+    public void firstTest() {
+        Matcher<View> viewMatcher = withId(R.id.mainName);
+        onView(viewMatcher).perform(typeText("coucou"));
+        onView(withId(R.id.mainGoButton)).perform(click());
+        intended(allOf(IntentMatchers.hasComponent(GreetingActivity.class.getName()),
+                IntentMatchers.hasExtra("key", "Hello coucou!")
+        ));
+
+        onView(withId(R.id.greetingMessage)).check(matches(withText("Hello coucou!")));
+    }
+}
--- app/src/main/java/com/sdpteam/connectout/MainActivity.java
+++ app/src/main/java/com/sdpteam/connectout/MainActivity.java
@@ -1,7 +1,10 @@
 package com.sdpteam.connectout;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText t = findViewById(R.id.mainName);
+        Button b = findViewById(R.id.mainGoButton);
+        b.setOnClickListener(v -> {
+            Intent myIntent = new Intent(getApplicationContext(), GreetingActivity.class);
+            String msg = "Hello " + t.getText().toString() + "!";
+            myIntent.putExtra("key", msg);
+            MainActivity.this.startActivity(myIntent);
+        });
     }
 }
