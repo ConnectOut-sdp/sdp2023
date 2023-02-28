790a6ccb45ffafd91ea8ba4182333d03ac5ae0fa
gamemike <100021446+ESmike0@users.noreply.github.com>
2023-02-28 21:16:45 +0100

    Add Drawer

--- app/src/main/java/com/sdpteam/connectout/DrawerActivity.java
+++ app/src/main/java/com/sdpteam/connectout/DrawerActivity.java
@@ -1,82 +1,107 @@
 package com.sdpteam.connectout;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Menu;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.sdpteam.connectout.databinding.ActivityDrawerBinding;
-import com.sdpteam.connectout.ui.filter.FilterFragment;
-import com.sdpteam.connectout.ui.home.HomeFragment;
-import com.sdpteam.connectout.ui.myAccount.MyAccountFragment;
-import com.sdpteam.connectout.ui.myEvents.MyEventsFragment;
+import com.sdpteam.connectout.fragments.FilterFragment;
+import com.sdpteam.connectout.fragments.HomeFragment;
+import com.sdpteam.connectout.fragments.MyAccountFragment;
+import com.sdpteam.connectout.fragments.MyEventsFragment;
+
 
 public class DrawerActivity extends AppCompatActivity {
-
     @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        com.sdpteam.connectout.databinding.ActivityDrawerBinding binding = ActivityDrawerBinding.inflate(getLayoutInflater());
+        //Find drawer's principal view and initiate it.
+        ActivityDrawerBinding binding = ActivityDrawerBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        //Retrieve tool bar and give it the control
         Toolbar toolbar  = binding.appBarDrawer.toolbar;
         setSupportActionBar(toolbar);
+
+        //Retrieve drawer and navigation view instances
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
 
 
-        toolbar.setNavigationOnClickListener(v -> drawer.open());
+        //Upon click on the tool bar icon, open the drawer of options
+        toolbar.setNavigationOnClickListener(v -> drawer.openDrawer(GravityCompat.START));
+
+        //Instantiate a drawer toggle, so upon listening to changes, the drawer closes or opens up.
         ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawer, R.string.open_drawer, R.string.close_drawer);
         drawer.addDrawerListener(drawerToggle);
-        drawerToggle.syncState(); //Is okay since in the main
 
+        // Upon button clicked of the drawer, replace current fragment with needed fragment
         navigationView.setNavigationItemSelectedListener(item -> {
 
+            //Show the item that is checked upon re-opening the drawer
             item.setChecked(true);
+            //closes the drawer upon receiving a click on an option
             drawer.close();
 
+            //Select next view change upon different options
             switch (item.getItemId()) {
-                case R.id.nav_home:
+                case R.id.menu_home:
+                    //Go back to the Map/List view
                     replaceFragment(new HomeFragment(),R.id.drawer_fragment_container);
                     break;
-                case R.id.account:
+                case R.id.menu_my_account:
+                    //Go check out your account
                     replaceFragment(new MyAccountFragment(), R.id.drawer_fragment_container);
                     break;
-                case R.id.myEvents:
+                case R.id.menu_my_events:
+                    //Go check out your events
                     replaceFragment(new MyEventsFragment(), R.id.drawer_fragment_container);
                     break;
-                case R.id.filters:
-                    replaceFragment(new FilterFragment(), R.id.drawer_fragment_container);
+                case R.id.menu_filters:
+                    //Add add to the Map/List view, a TextEdit in order to filter what is displayed
+                    FilterFragment filterFragment =  new FilterFragment();
+                    replaceFragment(filterFragment,R.id.drawer_fragment_container);
+                    break;
+                case R.id.menu_logout:
+                    Intent logOutIntent  = new Intent(getApplicationContext(), LogInActivity.class);
+                    startActivity(logOutIntent);
                     break;
             }
+
 
             return true;
         });
 
     }
 
+    /**
+     *
+     * @param fragment (Fragment): fragment to be used next
+     * @param idOfContainer (int): id of the container where the fragment is stored
+     */
     private void replaceFragment(Fragment fragment, int idOfContainer) {
 
+        //Retrieve the fragment's handler
         FragmentManager fragmentManager = getSupportFragmentManager();
+
+        //Initiate the swap of fragments
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(idOfContainer, fragment);
+
+        //Replace the current fragment with the next one
+        fragmentTransaction.add(idOfContainer, fragment);
         fragmentTransaction.commit();
     }
 
