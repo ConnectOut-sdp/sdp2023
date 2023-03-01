9a14c17c40e2e00502e58f1e7d37e2780869aa0e
gamemike <100021446+ESmike0@users.noreply.github.com>
2023-03-01 19:11:20 +0100

    Add tests

--- app/src/androidTest/java/com/sdpteam/connectout/DrawerActivityTest.java
+++ app/src/androidTest/java/com/sdpteam/connectout/DrawerActivityTest.java
@@ -1,17 +1,31 @@
 package com.sdpteam.connectout;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.DrawerMatchers.isClosed;
+import static androidx.test.espresso.contrib.DrawerMatchers.isOpen;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import android.widget.Toolbar;
+
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.espresso.contrib.NavigationViewActions;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.google.android.material.navigation.NavigationView;
 
 import org.junit.After;
 import org.junit.Before;
@@ -82,6 +96,16 @@ public class DrawerActivityTest {
         // Click on the Logout menu item
         onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.menu_logout));
         intended(hasComponent(LogInActivity.class.getName()));
+    }
+    
+    @Test
+    public void drawerIsClosedBeforeInteraction(){
+        onView(withId(R.id.drawer_layout)).check(matches(isClosed(GravityCompat.START)));
+    }
+    @Test
+    public void drawerIsOpenedAfterClick(){
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.drawer_layout)).check(matches(isOpen(GravityCompat.START)));
     }
 
 
