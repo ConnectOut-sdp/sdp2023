a2e97deb40e2860538748645310cf1c96a3fe835
David Kalajdzic <david.kalajdzic@icloud.com>
2023-03-05 19:52:29 +0100

    does google maps fragment test passes in CI or not?

--- app/src/androidTest/java/com/sdpteam/connectout/map/MapFragmentTest.java
+++ app/src/androidTest/java/com/sdpteam/connectout/map/MapFragmentTest.java
@@ -6,50 +6,31 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
-import androidx.fragment.app.testing.FragmentScenario;
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.intent.Intents;
-import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import com.sdpteam.connectout.MainActivity;
-import com.sdpteam.connectout.R;
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
+import com.sdpteam.connectout.R;
+
+import android.os.Handler;
+import android.os.Looper;
+import androidx.fragment.app.testing.FragmentScenario;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
 
 @RunWith(AndroidJUnit4.class)
+@LargeTest //without that does not seem to work :/
 public class MapFragmentTest {
 
-    private MainActivity activity;
-
-    @Before
-    public void setUp() {
-        Intents.init();
-    }
-
-    @After
-    public void tearDown() {
-        Intents.release();
-    }
-
-
     @Test
-    public void testMapDisplayed() {
+    public void testViewDisplayed() {
         FragmentScenario<MapFragment> scenario = FragmentScenario.launchInContainer(MapFragment.class);
         scenario.onFragment(fragment -> {
-            {
-                CompletableFuture.runAsync(() -> {
-                    onView(ViewMatchers.withId(R.id.map)).check(matches(isDisplayed()));
-                    onView(withId(R.id.refresh_button)).perform(click());
-                    onView(withId(R.id.map)).check(matches(isDisplayed()));
-                }).orTimeout(1, TimeUnit.MINUTES).join();
-            }
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(() -> {
+                onView(withId(R.id.map)).check(matches(isDisplayed()));
+                onView(withId(R.id.refresh_button)).perform(click());
+                onView(withId(R.id.map)).check(matches(isDisplayed()));
+            });
         });
     }
 }
