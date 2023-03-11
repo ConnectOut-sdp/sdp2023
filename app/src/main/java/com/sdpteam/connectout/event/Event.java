1e777471608f644b50f87bb2f68f6d9b5ae568eb
gamemike <100021446+ESmike0@users.noreply.github.com>
2023-03-11 22:01:37 +0100

    Add ownerId & eventId & refactor usage

--- app/src/main/java/com/sdpteam/connectout/event/Event.java
+++ app/src/main/java/com/sdpteam/connectout/event/Event.java
@@ -1,6 +1,7 @@
 package com.sdpteam.connectout.event;
 
 import com.sdpteam.connectout.map.GPSCoordinates;
+import com.sdpteam.connectout.profile.EditProfileActivity;
 
 /**
  * This class describes an event
@@ -9,13 +10,21 @@ import com.sdpteam.connectout.map.GPSCoordinates;
 public class Event {
 
     private final String title;
-    private final GPSCoordinates coordinates;
+    private final GPSCoordinates gpsCoordinates;
     private final String description;
+    private final String ownerId;
+    private final String eventId;
 
-    public Event(String title, GPSCoordinates coordinates, String description) {
+    public Event() {
+        this(null, null, null, EditProfileActivity.NULL_USER, null);
+    }
+
+    public Event(String title, GPSCoordinates gpsCoordinates, String description, String ownerId, String eventId) {
         this.title = title;
-        this.coordinates = coordinates;
+        this.gpsCoordinates = gpsCoordinates;
         this.description = description;
+        this.ownerId = ownerId;
+        this.eventId = eventId;
     }
 
     public String getDescription() {
@@ -26,8 +35,16 @@ public class Event {
         return title;
     }
 
-    public GPSCoordinates getGPSCoordinates() {
-        return coordinates;
+
+    public GPSCoordinates getGpsCoordinates() {
+        return gpsCoordinates;
     }
 
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
 }
