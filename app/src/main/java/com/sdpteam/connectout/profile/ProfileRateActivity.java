27ff6a84dd58aa8c5c973bacf16bfb41e1a12bb2
Lukovitch <79459584+Lukovitch@users.noreply.github.com>
2023-03-14 18:04:18 +0100

    starting

---
+++ app/src/main/java/com/sdpteam/connectout/profile/ProfileRateActivity.java
@@ -0,0 +1,16 @@
+package com.sdpteam.connectout.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sdpteam.connectout.R;
+
+public class ProfileRateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_rate);
+    }
+}
