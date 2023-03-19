d24ffc44ba4cd94991e13fdde8e978111f05807c
gamemike <100021446+ESmike0@users.noreply.github.com>
2023-03-20 00:11:41 +0100

    Add filtering

--- app/src/main/java/com/sdpteam/connectout/drawer/FilterFragment.java
+++ app/src/main/java/com/sdpteam/connectout/drawer/FilterFragment.java
@@ -1,21 +1,56 @@
 package com.sdpteam.connectout.drawer;
 
 import com.sdpteam.connectout.R;
+import com.sdpteam.connectout.userList.ProfileListOption;
+import com.sdpteam.connectout.userList.UserListFragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.ToggleButton;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 public class FilterFragment extends Fragment {
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    private final UserListFragment userListFragment;
 
-        //Add to this fragment, the home fragment to filter
-        this.getChildFragmentManager().beginTransaction().replace(R.id.filter_container, new HomeFragment()).commit();
-        return inflater.inflate(R.layout.fragment_filter, container, false);
+    public FilterFragment(){
+        userListFragment = new UserListFragment();
+    }
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_filter, container, false);
+
+        this.getChildFragmentManager().beginTransaction().replace(R.id.filter_container, userListFragment).commit();
+
+        ToggleButton categoryButton = rootView.findViewById(R.id.filter_category_button);
+        ToggleButton applicationButton = rootView.findViewById(R.id.filter_apply_button);
+        EditText text = rootView.findViewById(R.id.text_filter);
+
+        applicationButton.setOnClickListener(v-> {
+                    if (categoryButton.isChecked()) {
+                        userListFragment.changeObserved(ProfileListOption.NAME, Collections.singletonList(text.getText().toString()));
+                    } else {
+                        String[] rangeText = text.getText().toString().split(",");
+                        userListFragment.changeObserved(ProfileListOption.RATING, Arrays.stream(rangeText).collect(Collectors.toList()));
+                    }
+                }
+        );
+        return rootView;
+    }
+
+    public void stopObservation(){
+        userListFragment.stopObservation();
     }
 }
--- app/src/main/java/com/sdpteam/connectout/userList/UserListFragment.java
+++ app/src/main/java/com/sdpteam/connectout/userList/UserListFragment.java
@@ -1,5 +1,6 @@
 package com.sdpteam.connectout.userList;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,26 +8,84 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
 
+import com.sdpteam.connectout.PassedField;
 import com.sdpteam.connectout.R;
 import com.sdpteam.connectout.profile.Profile;
+import com.sdpteam.connectout.profile.ProfileActivity;
+import com.sdpteam.connectout.profile.ProfileModel;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserListFragment extends Fragment {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View contentView = inflater.inflate(R.layout.fragment_user_list, container, false);
-        UserListViewModel mViewModel = new UserListViewModel(new UserListModel());
-        List<Profile> sortedProfileList = mViewModel.getProfileSortedByRating();
+    private ListView listView;
+    private ViewGroup container;
+    private ProfilesAdapter profilesAdapter;
+    private final UserListViewModel viewModel;
+    private final Observer<List<Profile>> profilesObserver;
+    private LiveData<List<Profile>> observed;
 
-        ProfilesAdapter adapter = new ProfilesAdapter(container.getContext(),  R.layout.adapter_text_view, sortedProfileList);
-        ListView listView = contentView.findViewById(R.id.user_list_view);
-        listView.setAdapter(adapter);
+    public UserListFragment() {
+        profilesObserver = this::showProfileList;
+        viewModel = new UserListViewModel(new ProfileModel());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the view for this fragment in its container.
+        View contentView = inflater.inflate(R.layout.fragment_user_list, container, false);
+        this.container = container;
+
+        //Observe changes on the profile list.
+        changeObserved(ProfileListOption.NONE, null);
+
+        //Setup the profile Adapter, which will create the view for each given profile & give it to the view.
+        profilesAdapter = new ProfilesAdapter(container.getContext(), R.layout.adapter_text_view, new ArrayList<>());
+        listView = contentView.findViewById(R.id.user_list_view);
+        listView.setAdapter(profilesAdapter);
+
+        //On an item being clicked in the list, launch its profile view.
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            Intent profileIntent = new Intent(getContext(), ProfileActivity.class);
+            profileIntent.putExtra(PassedField.UserId.toString(), profilesAdapter.getItem(position).getId());
+            startActivity(profileIntent);
+        });
         return contentView;
+    }
+
+    /**
+     * Update the list of profiles in the view.
+     *
+     * @param profiles (List<Profile>): new list of profiles to update.
+     */
+    public void showProfileList(List<Profile> profiles) {
+        //Filter out all elements with null names
+        profiles = profiles.stream().filter(p -> p.getName() != null).collect(Collectors.toList());
+
+        //Setup the view with the new Adapter containing the updated list.
+        profilesAdapter = new ProfilesAdapter(container.getContext(), R.layout.adapter_text_view, profiles);
+        listView.setAdapter(profilesAdapter);
+    }
+
+    public void changeObserved(ProfileListOption option, List<String> arguments) {
+        if (observed != null) {
+            observed.removeObserver(profilesObserver);
+        }
+
+        observed = viewModel.getListOfProfile(option, arguments);
+        observed.observe(getViewLifecycleOwner(), profilesObserver);
+
+    }
+
+    public void stopObservation() {
+        if (observed != null) {
+            observed.removeObserver(profilesObserver);
+        }
     }
 
 
