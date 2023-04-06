3326ce8b1adcc22c9629296c96ab1665e15318b4
gamemike <100021446+ESmike0@users.noreply.github.com>
2023-04-06 18:39:14 +0200

    Add ParticipantsFilter

---
+++ app/src/main/java/com/sdpteam/connectout/event/nearbyEvents/filter/ParticipantsFilter.java
@@ -0,0 +1,23 @@
+package com.sdpteam.connectout.event.nearbyEvents.filter;
+
+import com.sdpteam.connectout.profile.Profile;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public interface ParticipantsFilter extends Predicate<List<Profile>> {
+
+    ParticipantsFilter NONE = e -> true;
+    @Override
+    boolean test(List<Profile> profiles);
+
+    default ParticipantsFilter and(ParticipantsFilter other) {
+        return e -> test(e) && other.test(e);
+    }
+    default ParticipantsFilter or(ParticipantsFilter other) {
+        return e -> test(e) || other.test(e);
+    }
+    default ParticipantsFilter negate() {
+        return e -> !test(e);
+    }
+}
