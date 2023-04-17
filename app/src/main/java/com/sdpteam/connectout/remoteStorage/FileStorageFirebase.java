dee48ad6eb31aba0be9cad34ad2c252eef2f36ec
David Kalajdzic <david.kalajdzic@icloud.com>
2023-04-18 01:44:25 +0200

    upload File logs

--- app/src/main/java/com/sdpteam/connectout/remoteStorage/FileStorageFirebase.java
+++ app/src/main/java/com/sdpteam/connectout/remoteStorage/FileStorageFirebase.java
@@ -7,6 +7,7 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import android.net.Uri;
+import android.util.Log;
 
 public class FileStorageFirebase {
 
@@ -21,15 +22,28 @@ public class FileStorageFirebase {
      * @return the url of the uploaded file
      */
     public CompletableFuture<Uri> uploadFile(Uri file, String fileExtension) {
+        Log.e("FileStorageFirebase", "start uploading " + file.getPath());
+
         StorageReference fileReference = firebaseFilesStorage.child(System.currentTimeMillis() + "_" + UUID.randomUUID() + "." + fileExtension);
         CompletableFuture<Uri> result = new CompletableFuture<>();
         fileReference.putFile(file)
                 .addOnCompleteListener(taskSnapshot -> {
-                    // Get the URL of the uploaded file
-                    fileReference.getDownloadUrl().addOnCompleteListener(task -> {
-                                result.complete(task.isSuccessful() ? task.getResult() : null);
+                    if (taskSnapshot.isSuccessful()) {
+                        fileReference.getDownloadUrl().addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Uri downloadUri = task.getResult();
+                                result.complete(downloadUri);
+                            } else {
+                                Log.e("FileStorageFirebase", "Error getting download URL", task.getException());
+                                System.out.println("FileStorageFirebase" + "Error getting download URL" + task.getException().toString());
+                                result.complete(null);
                             }
-                    );
+                        });
+                    } else {
+                        Log.e("FileStorageFirebase", "Error uploading file", taskSnapshot.getException());
+                        System.out.println("FileStorageFirebase" + "Error uploading file" + taskSnapshot.getException().toString());
+                        result.complete(null);
+                    }
                 });
         return result;
     }
