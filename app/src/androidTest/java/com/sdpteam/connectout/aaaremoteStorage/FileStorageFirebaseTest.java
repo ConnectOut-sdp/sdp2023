1402252f1db1911ae7063b66cb214037aefcf964
David Kalajdzic <david.kalajdzic@icloud.com>
2023-04-18 01:30:28 +0200

    is this the issue in ci?

--- app/src/androidTest/java/com/sdpteam/connectout/aaaremoteStorage/FileStorageFirebaseTest.java
+++ app/src/androidTest/java/com/sdpteam/connectout/aaaremoteStorage/FileStorageFirebaseTest.java
@@ -1,22 +1,20 @@
 package com.sdpteam.connectout.aaaremoteStorage;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import com.sdpteam.connectout.MainActivity;
-import com.sdpteam.connectout.R;
 import com.sdpteam.connectout.remoteStorage.FileStorageFirebase;
 import com.sdpteam.connectout.utils.TestActivity;
 
-import android.content.ContentResolver;
-import android.content.res.Resources;
 import android.net.Uri;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 @RunWith(AndroidJUnit4.class)
@@ -27,17 +25,32 @@ public class FileStorageFirebaseTest {
 
     @Test
     public void testUploadImageToFirebase() {
-        Resources resources = intentsTestRule.getActivity().getResources();
+//        Resources resources = intentsTestRule.getActivity().getResources();
+//
+//        // Create the resource Uri for the event_image drawable
+//        Uri uri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" +
+//                resources.getResourcePackageName(R.drawable.event_image) + '/' +
+//                resources.getResourceTypeName(R.drawable.event_image) + '/' +
+//                resources.getResourceEntryName(R.drawable.event_image));
+//        if (uri == null) {
+//            throw new IllegalStateException("Why is it null?");
+//        }
+        try {
+            File tempFile = File.createTempFile("test", ".jpg");
+            tempFile.deleteOnExit();
 
-        // Create the resource Uri for the event_image drawable
-        Uri uri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" +
-                resources.getResourcePackageName(R.drawable.event_image) + '/' +
-                resources.getResourceTypeName(R.drawable.event_image) + '/' +
-                resources.getResourceEntryName(R.drawable.event_image));
-        if (uri == null) {
-            throw new IllegalStateException("Why is it null?");
+            // Write some test data to the file
+            FileWriter writer = new FileWriter(tempFile);
+            writer.write("This is a test file.");
+            writer.close();
+
+            // Upload the file to Firebase Storage
+            Uri uri = Uri.fromFile(tempFile);
+
+            Uri createdUrl = new FileStorageFirebase().uploadFile(uri, "jpg").join();
+            assertTrue(createdUrl.toString().contains("https://"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        Uri createdUrl = new FileStorageFirebase().uploadFile(uri, "jpg").join();
-        assertTrue(createdUrl.toString().contains("https://"));
     }
 }
