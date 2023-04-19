8be2af430b2510667fbebda0846fad5bbeffb8cf
gamemike <100021446+ESmike0@users.noreply.github.com>
2023-04-19 19:47:39 +0200

    Add participation status change retrieval

--- app/src/main/java/com/sdpteam/connectout/event/viewer/EventViewModel.java
+++ app/src/main/java/com/sdpteam/connectout/event/viewer/EventViewModel.java
@@ -1,5 +1,6 @@
 package com.sdpteam.connectout.event.viewer;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
@@ -37,18 +38,20 @@ public class EventViewModel extends ViewModel {
      * Updates the participation status of the specified user in the event to "attending".
      *
      * @param userId (String): id of the user to add to the event
+     * @return (LiveData<Boolean>): true if participation status has been joined.
      */
-    public void joinEvent(String userId) {
-        updateParticipationStatus(userId, true);
+    public LiveData<Boolean> joinEvent(String userId) {
+        return updateParticipationStatus(userId, true);
     }
 
     /**
      * Updates the participation status of the specified user in the event to "not attending".
      *
      * @param userId (String): id of the user to remove from the event
+     * @return (LiveData<Boolean>): true if participation status has been left.
      */
-    public void leaveEvent(String userId) {
-        updateParticipationStatus(userId, false);
+    public LiveData<Boolean> leaveEvent(String userId) {
+       return updateParticipationStatus(userId, false);
     }
 
     /**
@@ -56,10 +59,13 @@ public class EventViewModel extends ViewModel {
      * if the user is attending/left the event, it removes/add the user from the event.
      *
      * @param userId (String): id of the user whose participation status needs to be toggled
+     * @return (LiveData<Boolean>): upon update, true if participation status has been changed.
      */
-    public void toggleParticipation(String userId) {
+    public LiveData<Boolean> toggleParticipation(String userId) {
+        MutableLiveData<Boolean> result = new MutableLiveData<>();
         eventRepository.getEvent(lastEventId).thenAccept(event ->
-                updateParticipationStatus(userId, !event.getParticipants().contains(userId)));
+                updateParticipationStatus(userId, !event.getParticipants().contains(userId)).observeForever(result::setValue));
+        return result;
     }
 
     /**
@@ -67,15 +73,26 @@ public class EventViewModel extends ViewModel {
      *
      * @param userId (String): id of the user whose participation status needs to be updated
      * @param isParticipating (boolean): true if the user is attending the event, false otherwise
+     * @return (LiveData<Boolean>): upon update, true if participation status has been changed.
      */
-    private void updateParticipationStatus(String userId, boolean isParticipating) {
+    private LiveData<Boolean> updateParticipationStatus(String userId, boolean isParticipating) {
+        MutableLiveData<Boolean> result = new MutableLiveData<>();
         // Check if an event is selected
         if (lastEventId != null) {
             if (isParticipating) {
-                eventRepository.joinEvent(lastEventId, userId).thenAccept(b -> refreshEvent());
+                eventRepository.joinEvent(lastEventId, userId).thenAccept(b ->
+                        {
+                            refreshEvent();
+                            result.setValue(b);
+                        }
+                );
             } else {
-                eventRepository.leaveEvent(lastEventId, userId).thenAccept(b -> refreshEvent());
+                eventRepository.leaveEvent(lastEventId, userId).thenAccept(b -> {
+                    refreshEvent();
+                    result.setValue(b);
+                });
             }
         }
+        return result;
     }
 }
