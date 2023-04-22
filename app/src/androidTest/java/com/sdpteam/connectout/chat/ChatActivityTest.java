d17be4d5146f39980cf7ffc032a529c72c2c26ff
David Kalajdzic <david.kalajdzic@icloud.com>
2023-04-22 13:23:59 +0200

    quick fix done

--- app/src/androidTest/java/com/sdpteam/connectout/chat/ChatActivityTest.java
+++ app/src/androidTest/java/com/sdpteam/connectout/chat/ChatActivityTest.java
@@ -10,8 +10,15 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.anything;
 
-import android.content.Intent;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import com.sdpteam.connectout.R;
+
+import android.content.Intent;
 import android.os.SystemClock;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.intent.Intents;
@@ -19,38 +26,31 @@ import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import com.sdpteam.connectout.R;
-import com.sdpteam.connectout.chat.ChatActivity;
-import com.sdpteam.connectout.chat.ChatFirebaseDataSource;
-import com.sdpteam.connectout.chat.ChatMessage;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
 @RunWith(AndroidJUnit4.class)
 public class ChatActivityTest {
     String nameOfTestChat = "TestChat";
     @Rule
     public ActivityScenarioRule<ChatActivity> activityRule = new ActivityScenarioRule<>(new Intent(ApplicationProvider.getApplicationContext(), ChatActivity.class).putExtra("chatId", nameOfTestChat));
 
+    ChatFirebaseDataSource model = new ChatFirebaseDataSource();
+
     @Before
     public final void setUp() {
         Intents.init();
+        model.emptyTestMode();
+        SystemClock.sleep(1000);
     }
 
     @After
     public final void tearDown() {
         Intents.release();
+        model.emptyTestMode();
+        SystemClock.sleep(1000);
     }
 
     @Test
     public void testListUsersDisplayed() {
         onView(ViewMatchers.withId(R.id.activity_chat)).check(matches(isDisplayed()));
-        ChatFirebaseDataSource model = new ChatFirebaseDataSource();
-        model.emptyTestMode();
         model.saveMessage(new ChatMessage("Bob", "Bob userId", "Hey I m Bob", nameOfTestChat));
         model.saveMessage(new ChatMessage("Alice", "Alice userId", "Hey I m Alice", nameOfTestChat));
         model.saveMessage(new ChatMessage("Dylan", "Dylan userId", "Hey I m Dylan", nameOfTestChat));
@@ -72,15 +72,14 @@ public class ChatActivityTest {
         onView(withId(R.id.chat_fab)).perform(click());
         SystemClock.sleep(1000);
         checkListViewValue(6, "Hi beautiful people", "You");
-        model.emptyTestMode();
     }
 
     public void checkListViewValue(int position, String text, String name) {
         onData(anything()).inAdapterView(withId(R.id.list_of_messages)).atPosition(position).
-                onChildView(withId(R.id.message_text)).
-                check(matches(withText(text)));
+                onChildView(withId(R.id.message_text))
+                .check(matches(withText(text)));
         onData(anything()).inAdapterView(withId(R.id.list_of_messages)).atPosition(position).
-                onChildView(withId(R.id.message_user)).
-                check(matches(withText(name)));
+                onChildView(withId(R.id.message_user))
+                .check(matches(withText(name)));
     }
 }
