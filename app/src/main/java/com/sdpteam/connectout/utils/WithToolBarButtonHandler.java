08d140ed1df7e2a6dc6941e064d3999a49f746b9
gamemike <100021446+ESmike0@users.noreply.github.com>
2023-04-23 16:07:14 +0200

    Add handler

---
+++ app/src/main/java/com/sdpteam/connectout/utils/WithToolBarButtonHandler.java
@@ -0,0 +1,9 @@
+package com.sdpteam.connectout.utils;
+
+import android.view.View;
+
+public interface WithToolBarButtonHandler {
+    public void setupButton(String text, View.OnClickListener listener);
+
+
+}
