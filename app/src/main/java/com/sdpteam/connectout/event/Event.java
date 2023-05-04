6a82062044a808167ade4b2cd083cb96d6fa292c
mikael <mikael@kalajdzic.ch>
2023-05-04 18:50:16 +0200

    adding back my changes, due to bad conflict solving

--- app/src/main/java/com/sdpteam/connectout/event/Event.java
+++ app/src/main/java/com/sdpteam/connectout/event/Event.java
@@ -5,7 +5,6 @@ import static com.sdpteam.connectout.profile.EditProfileActivity.NULL_USER;
 import com.sdpteam.connectout.event.nearbyEvents.map.GPSCoordinates;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.GregorianCalendar;
 import java.util.List;
 
@@ -15,78 +14,65 @@ import java.util.List;
  */
 public class Event {
 
-    /**A Profile must satisfy all these restrictions to be able to join an event*/
-    public static class EventRestrictions {
-        /**used to inform the user of the restriction that he does not satisfy when refused registration*/
-        public enum RestrictionStatus{
-            ALL_RESTRICTIONS_SATISFIED("Thank you for your registration"),
-            INSUFFICIENT_RATING("Registration blocked due to insufficient rating"),
-            MAX_NUM_PARTICIPANTS_REACHED("Registration blocked because the event is full"),
-
-            JOINING_DEADLINE_PASSED("The registration deadline has passed");
-            private String message;
-            RestrictionStatus(String message){
-                this.message = message;
-            }
-            public String getMessage(){return message;}
-        }
-        private final double minRating;
-
-        private final int maxNumParticipants;
-        private final long joiningDeadline;
-
-        public EventRestrictions(){
-            this(-1, Integer.MAX_VALUE, new GregorianCalendar(2100,1, 1).getTime().getTime());
-        }
-
-        public EventRestrictions(double minRating, int maxNumParticipants, long joiningDeadline){
-            this.minRating = minRating;
-            this.maxNumParticipants = maxNumParticipants;
-            this.joiningDeadline = joiningDeadline;
-        }
-
-        public double getMinRating(){return minRating;}
-
-        public int getMaxNumParticipants(){return maxNumParticipants;}
-        public long getJoiningDeadline(){return joiningDeadline;}
-    }
     public static final Event NULL_EVENT = new Event();
-
     private final String id;
     private final String title;
     private final String description;
     private final GPSCoordinates coordinates;
     private final String organizer;
     private final List<String> participants;
+    private final List<String> interestedParticipants;
     private final long date;
     private final EventRestrictions restrictions;
-
     private Event() {
-        this(NULL_USER, "NullTitle", "NullDescription", new GPSCoordinates(0, 0), NULL_USER, new ArrayList<>(), 0);
+        this(NULL_USER, "NullTitle", "NullDescription", new GPSCoordinates(0, 0), NULL_USER, new ArrayList<>(), new ArrayList<>(), 0);
     }
 
     public Event(String id, String title, String description, GPSCoordinates coordinates, String organizer) {
-        this(id, title, description, coordinates, organizer, new ArrayList<>(), 0);
+        this(id, title, description, coordinates, organizer, new ArrayList<>(), new ArrayList<>(), 0);
     }
 
-
-    public Event(String id, String title, String description, GPSCoordinates coordinates, String organizer, List<String> participants) {
-        this(id, title, description, coordinates, organizer, participants, 0);
+    public Event(String id,
+                 String title,
+                 String description,
+                 GPSCoordinates coordinates,
+                 String organizer,
+                 List<String> participants,
+                 List<String> interestedParticipants) {
+        this(id, title, description, coordinates, organizer, participants, interestedParticipants, 0);
     }
 
-    public Event(String id, String title, String description, GPSCoordinates coordinates, String organizer,List<String> participants, long date) {
-        this(id, title, description, coordinates, organizer, participants, date, new EventRestrictions());
+    public Event(String id,
+                 String title,
+                 String description,
+                 GPSCoordinates coordinates,
+                 String organizer,
+                 List<String> participants,
+                 List<String> interestedParticipants,
+                 long date) {
+        this(id, title, description, coordinates, organizer, participants, interestedParticipants, date, new EventRestrictions());
     }
-    public Event(String id, String title, String description, GPSCoordinates coordinates, String organizer,List<String> participants, long date, EventRestrictions restrictions) {
+
+    public Event(String id,
+                 String title,
+                 String description,
+                 GPSCoordinates coordinates,
+                 String organizer,
+                 List<String> participants,
+                 List<String> interestedParticipants,
+                 long date,
+                 EventRestrictions restrictions) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.coordinates = coordinates;
         this.organizer = organizer;
-        this.date = date;
         this.participants = participants;
+        this.interestedParticipants = interestedParticipants;
+        this.date = date;
         this.restrictions = restrictions;
     }
+
     public String getId() {
         return id;
     }
@@ -111,27 +97,114 @@ public class Event {
         return participants;
     }
 
-    public long getDate(){return date;}
+    public List<String> getInterestedParticipants() {
+        return interestedParticipants;
+    }
 
-    public EventRestrictions getRestrictions(){return restrictions;}
+    public boolean hasJoined(String userId) {
+        return participants.contains(userId);
+    }
+
+    public boolean isInterested(String userId) {
+        return interestedParticipants.contains(userId);
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public EventRestrictions getRestrictions() {
+        return restrictions;
+    }
+
     /**
+     * Adds a participant to the list. Does nothing if already in the list. Removes it from the
+     * interested participants list.
      *
      * @param id (String): id of the participant
-     * @return (boolean): true if the participants was added.
+     * @return (boolean): true if the participants was successfully added.
      */
     public boolean addParticipant(String id) {
-        boolean absent = !participants.contains(id);
-        if(absent){
-            participants.add(id);
+        if (participants.contains(id)) {
+            return false;
         }
-        return absent;
+        participants.add(id);
+        interestedParticipants.remove(id);
+        return true;
     }
+
     /**
+     * Adds a participants to the interested list. Does nothing if already joined the event.
+     *
+     * @param id of the participant
+     * @return true if the participants was successfully added to the interested list
+     */
+    public boolean addInterestedParticipant(String id) {
+        if (participants.contains(id) || interestedParticipants.contains(id)) {
+            return false;
+        }
+        interestedParticipants.add(id);
+        return true;
+    }
+
+    /**
+     * Completely removes the participant from list and interested list as well
      *
      * @param id (String): id of the participant
      * @return (boolean): true if the participants was removed.
      */
     public boolean removeParticipant(String id) {
+        interestedParticipants.remove(id);
         return participants.remove(id);
+    }
+
+    /**
+     * A Profile must satisfy all these restrictions to be able to join an event
+     */
+    public static class EventRestrictions {
+        private final double minRating;
+        private final int maxNumParticipants;
+        private final long joiningDeadline;
+        public EventRestrictions() {
+            this(-1, Integer.MAX_VALUE, new GregorianCalendar(2100, 1, 1).getTime().getTime());
+        }
+
+        public EventRestrictions(double minRating, int maxNumParticipants, long joiningDeadline) {
+            this.minRating = minRating;
+            this.maxNumParticipants = maxNumParticipants;
+            this.joiningDeadline = joiningDeadline;
+        }
+
+        public double getMinRating() {
+            return minRating;
+        }
+
+        public int getMaxNumParticipants() {
+            return maxNumParticipants;
+        }
+
+        public long getJoiningDeadline() {
+            return joiningDeadline;
+        }
+
+        /**
+         * used to inform the user of the restriction that he does not satisfy when refused registration
+         */
+        public enum RestrictionStatus {
+            ALL_RESTRICTIONS_SATISFIED("Thank you for your registration"),
+            INSUFFICIENT_RATING("Registration blocked due to insufficient rating"),
+            MAX_NUM_PARTICIPANTS_REACHED("Registration blocked because the event is full"),
+
+            JOINING_DEADLINE_PASSED("The registration deadline has passed");
+            private final String message;
+
+            RestrictionStatus(String message) {
+                this.message = message;
+            }
+
+            public String getMessage() {
+                return message;
+            }
+        }
     }
 }
