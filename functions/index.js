6c044a48848f6fef7e599cf4b40266750b344d7e
Thösam <thosamtenzin2002@gmail.com>
2023-05-05 02:41:42 +0200

    scheduling notifications as campaign

--- functions/index.js
+++ functions/index.js
@@ -8,71 +8,45 @@ exports.sendEventReminders = functions.database.ref("/Events/{eventId}")
       const eventId = context.params.eventId;
 
       // Get the time of the event and calculate the times for the reminders
-      const eventTime = new Date(eventData.time);
+      const eventTime = new Date(eventData.date);
       const oneDayBefore = new Date(eventTime.getTime() -
       (24 * 60 * 60 * 1000));
       const twoHoursBefore = new Date(eventTime.getTime() -
       (2 * 60 * 60 * 1000));
 
-      // Calculate the delays until each reminder should be sent
-      const delayUntilOneDayBefore = Math.max(0,
-          oneDayBefore.getTime() - Date.now());
-      const delayUntilTwoHoursBefore = Math.max(0,
-          twoHoursBefore.getTime() - Date.now());
-      const delayUntilEventTime = Math.max(0, eventTime.getTime() - Date.now());
-
       // Schedule the notifications
-      setTimeout(() => {
-        const message = {
-          notification: {
-            title: `Upcoming event: ${eventData.title}`,
-            body: `Event "${eventData.title}" is happening now`,
-          },
-          topic: "event_" + eventId,
-        };
+      const oneDayBeforeJob = admin.pubsub.schedule(new Date(oneDayBefore))
+          .topic(`event_${eventId}`)
+          .message({
+            notification: {
+              title: `Upcoming event: ${eventData.title}`,
+              body: `Event "${eventData.title}" is happening tomorrow`,
+            },
+          })
+          .schedule();
 
-        admin.messaging().send(message)
-            .then((response) => {
-              console.log("Successfully sent event reminder:", response);
-            })
-            .catch((error) => {
-              console.log("Error sending event reminder:", error);
-            });
-      }, delayUntilEventTime);
+      const twoHoursBeforeJob = admin.pubsub.schedule(new Date(twoHoursBefore))
+          .topic(`event_${eventId}`)
+          .message({
+            notification: {
+              title: `Upcoming event: ${eventData.title}`,
+              body: `Event "${eventData.title}" is happening in 2 hours`,
+            },
+          })
+          .schedule();
 
-      setTimeout(() => {
-        const message = {
-          notification: {
-            title: `Upcoming event: ${eventData.title}`,
-            body: `Event "${eventData.title}" is happening in 2 hours`,
-          },
-          topic: "event_" + eventId,
-        };
+      const eventTimeJob = admin.pubsub.schedule(new Date(eventTime))
+          .topic(`event_${eventId}`)
+          .message({
+            notification: {
+              title: `Upcoming event: ${eventData.title}`,
+              body: `Event "${eventData.title}" is happening now`,
+            },
+          })
+          .schedule();
 
-        admin.messaging().send(message)
-            .then((response) => {
-              console.log("Successfully sent 2-hour reminder:", response);
-            })
-            .catch((error) => {
-              console.log("Error sending 2-hour reminder:", error);
-            });
-      }, delayUntilTwoHoursBefore);
-
-      setTimeout(() => {
-        const message = {
-          notification: {
-            title: `Upcoming event: ${eventData.title}`,
-            body: `Event "${eventData.title}" is happening tomorrow`,
-          },
-          topic: "event_" + eventId,
-        };
-
-        admin.messaging().send(message)
-            .then((response) => {
-              console.log("Successfully sent 1-day reminder:", response);
-            })
-            .catch((error) => {
-              console.log("Error sending 1-day reminder:", error);
-            });
-      }, delayUntilOneDayBefore);
+      console.log(`Scheduled event reminders for event_${eventId}:`);
+      console.log(`1 day before: ${oneDayBeforeJob}`);
+      console.log(`2 hours before: ${twoHoursBeforeJob}`);
+      console.log(`Event time: ${eventTimeJob}`);
     });
