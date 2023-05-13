cd7cff953cbc7bb750996db4045c5f6f40b7da30
gamemike <100021446+ESmike0@users.noreply.github.com>
2023-05-14 00:53:09 +0200

    Add simplified datasource

--- app/src/main/java/com/sdpteam/connectout/post/model/PostFirebaseDataSource.java
+++ app/src/main/java/com/sdpteam/connectout/post/model/PostFirebaseDataSource.java
@@ -3,17 +3,26 @@ package com.sdpteam.connectout.post.model;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
+import com.google.firebase.database.FirebaseDatabase;
 import com.sdpteam.connectout.utils.Result;
 
 public class PostFirebaseDataSource implements PostDataSource {
+    private static final String POSTS = "Posts";
+
+    //TODO fix through firebase task
     @Override
     public CompletableFuture<Result<Void>> savePost(Post post) {
-        return CompletableFuture.completedFuture(new Result<>(null, false, "Not implemented"));
+        String newId = FirebaseDatabase.getInstance().getReference().child(POSTS).push().getKey();
+        Post newPost = new Post(newId, post.getProfileId(), post.getEventId(), post.getCommentsChatId(), post.getImagesUrls(), post.getNbrLikes(), post.getVisibility(), post.getTitle(), post.getDescription());
+        FirebaseDatabase.getInstance().getReference().child(POSTS).child(newPost.getId()).setValue(newPost);
+        return CompletableFuture.completedFuture(null);
     }
 
+    //TODO fix through firebase task
     @Override
     public CompletableFuture<Result<Void>> deletePost(String postId) {
-        return CompletableFuture.completedFuture(new Result<>(null, false, "Not implemented"));
+        FirebaseDatabase.getInstance().getReference().child(POSTS).child(postId).removeValue();
+        return CompletableFuture.completedFuture(null);
     }
 
     @Override
