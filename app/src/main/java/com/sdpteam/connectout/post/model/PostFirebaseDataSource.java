3f0b27d78717d454607c39a404c306eda0027ebb
gamemike <100021446+ESmike0@users.noreply.github.com>
2023-05-14 02:00:30 +0200

    Add basic firebase

--- app/src/main/java/com/sdpteam/connectout/post/model/PostFirebaseDataSource.java
+++ app/src/main/java/com/sdpteam/connectout/post/model/PostFirebaseDataSource.java
@@ -1,9 +1,15 @@
 package com.sdpteam.connectout.post.model;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.FirebaseDatabase;
+import com.sdpteam.connectout.event.Event;
+import com.sdpteam.connectout.event.EventFirebaseDataSource;
+import com.sdpteam.connectout.profile.ProfileFirebaseDataSource;
 import com.sdpteam.connectout.utils.Result;
 
 public class PostFirebaseDataSource implements PostDataSource {
@@ -30,18 +36,33 @@ public class PostFirebaseDataSource implements PostDataSource {
         return CompletableFuture.completedFuture(new Result<>(null, false, "Not implemented"));
     }
 
+    //TODO fix through firebase task
     @Override
     public CompletableFuture<Result<List<Post>>> fetchAllPosts(String userId) {
-        return CompletableFuture.completedFuture(new Result<>(null, false, "Not implemented"));
+        CompletableFuture<Result<List<Post>>> future = new CompletableFuture<>();
+        FirebaseDatabase.getInstance().getReference().child(POSTS)
+                .orderByKey()
+                .get()
+                .addOnCompleteListener(t -> {
+
+                    List<Post> posts = new ArrayList<>();
+                    for (DataSnapshot child : t.getResult().getChildren()) {
+                        Post post = child.getValue(Post.class);
+                        posts.add(post);
+                    }
+                    future.complete(new Result<>(posts, true, ""));
+                });
+
+        return future;
     }
 
     @Override
     public CompletableFuture<Result<List<Post>>> fetchAllPostsOfEvent(String userId, String eventId) {
-        return CompletableFuture.completedFuture(new Result<>(null, false, "Not implemented"));
+        return fetchAllPosts(userId);
     }
 
     @Override
     public CompletableFuture<Result<List<Post>>> fetchPostMadeByUser(String userId, String authorId) {
-        return CompletableFuture.completedFuture(new Result<>(null, false, "Not implemented"));
+        return fetchAllPosts(userId);
     }
 }
