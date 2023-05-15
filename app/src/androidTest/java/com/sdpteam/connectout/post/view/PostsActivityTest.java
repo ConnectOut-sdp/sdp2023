9dd467a5883048c65272db68c17d55c93ece2806
Lukovitch <79459584+Lukovitch@users.noreply.github.com>
2023-05-15 20:51:47 +0200

    add more tests

--- app/src/androidTest/java/com/sdpteam/connectout/post/view/PostsActivityTest.java
+++ app/src/androidTest/java/com/sdpteam/connectout/post/view/PostsActivityTest.java
@@ -1,10 +1,15 @@
 package com.sdpteam.connectout.post.view;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static com.sdpteam.connectout.chat.comment.CommentsActivity.PASSED_COMMENTS_KEY;
 import static com.sdpteam.connectout.utils.FutureUtils.waitABit;
 import static com.sdpteam.connectout.utils.WithIndexMatcher.withIndex;
 
@@ -15,6 +20,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.sdpteam.connectout.R;
+import com.sdpteam.connectout.chat.comment.CommentsActivity;
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 import androidx.test.espresso.intent.Intents;
@@ -57,6 +63,7 @@ public class PostsActivityTest {
     public void commentsButtonDisplayedAndClickable() {
         onView(withId(R.id.posts_list_view)).check(matches(isDisplayed()));
         onView(withIndex(withId(R.id.post_comments_button), 0)).perform(click()); // click this button on the first post in the list
+        intended(hasComponent(CommentsActivity.class.getName()));
     }
 
     @Test
