6146556f11614b1c39565a5242358c4cc1ef8749
TheAmericanMomo <aymericdechillaz@gmail.com>
2023-05-25 23:52:22 +0200

    fixed import issue

--- app/src/main/java/com/sdpteam/connectout/MainActivity.java
+++ app/src/main/java/com/sdpteam/connectout/MainActivity.java
@@ -1,17 +1,13 @@
 package com.sdpteam.connectout;
 
-import com.google.firebase.database.FirebaseDatabase;
-import com.sdpteam.connectout.authentication.GoogleLoginActivity;
-import com.sdpteam.connectout.drawer.DrawerActivity;
-import com.sdpteam.connectout.event.viewer.MyEventsCalendarActivity;
-import com.sdpteam.connectout.notifications.NotificationService;
-import com.sdpteam.connectout.post.view.PostsActivity;
-import com.sdpteam.connectout.registration.CompleteRegistration;
-
-
 import android.content.Intent;
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.sdpteam.connectout.authentication.GoogleLoginActivity;
+import com.sdpteam.connectout.notifications.NotificationService;
 
 public class MainActivity extends AppCompatActivity {
 
