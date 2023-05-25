6fd4797e33d2472a1d9b4671e4610f76290ae329
David Kalajdzic <david.kalajdzic@icloud.com>
2023-05-25 22:23:52 +0200

    coverage increased

--- app/src/androidTest/java/com/sdpteam/connectout/utils/NullStringUtilTest.java
+++ app/src/androidTest/java/com/sdpteam/connectout/utils/NullStringUtilTest.java
@@ -1,13 +1,25 @@
 package com.sdpteam.connectout.utils;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 import org.junit.Test;
 
 public class NullStringUtilTest {
     @Test
-    public void quickTest() {
+    public void nullStringGivesEmptyString() {
         assertEquals("", NullStringUtil.nonNullString(null));
+    }
+
+    @Test
+    public void defaultValueWorks() {
         assertEquals("default", NullStringUtil.nonNullString(null, "default"));
+    }
+
+    @Test
+    public void nullDefaultValueShouldNotBeNull() {
+        assertThrows(NullPointerException.class, () -> {
+            String s = NullStringUtil.nonNullString(null, null);
+        });
     }
 }
