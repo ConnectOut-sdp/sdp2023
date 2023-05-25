068f218af155943cc4fec2a032d59280ef2effc5
David Kalajdzic <david.kalajdzic@icloud.com>
2023-05-25 02:06:36 +0200

    coverage increased

--- app/src/androidTest/java/com/sdpteam/connectout/post/view/PostsActivityTest.java
+++ app/src/androidTest/java/com/sdpteam/connectout/post/view/PostsActivityTest.java
@@ -3,6 +3,8 @@ package com.sdpteam.connectout.post.view;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static com.sdpteam.connectout.profile.EditProfileActivity.NULL_USER;
@@ -23,6 +25,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.sdpteam.connectout.R;
+import com.sdpteam.connectout.chat.comment.CommentsActivity;
 import com.sdpteam.connectout.post.model.Post;
 import com.sdpteam.connectout.post.model.PostFirebaseDataSource;
 
@@ -53,10 +56,8 @@ public class PostsActivityTest {
     @BeforeClass
     public static void setUpClass() {
         imagesUrls.clear();
-        imagesUrls.add("https://firebasestorage.googleapis.com/v0/b/my-project-test-a847f.appspot.com/o/1683269814271_40078ef6-3886-4c15-b013-940b8d3f1614" +
-                ".jpg?alt=media&token=c5abae50-d2af-4361-bb75-d975f8460a6d");
-        imagesUrls.add("https://firebasestorage.googleapis.com/v0/b/my-project-test-a847f.appspot.com/o/1683269290710_5e3659ce-9aea-4f90-aea2-64eb941d6a6c" +
-                ".jpg?alt=media&token=590a4df0-12c1-491a-9b89-f621c8f2bbe3");
+        imagesUrls.add("https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png");
+        imagesUrls.add("https://www.wikipedia.org/portal/wikipedia.org/assets/img/Wikipedia-logo-v2@2x.png");
 
         assertTrue(fJoin(new PostFirebaseDataSource().savePost(TEST_POST_EVENT)).isSuccess());
         assertTrue(fJoin(new PostFirebaseDataSource().savePost(TEST_POST_EVENT1)).isSuccess());
@@ -85,33 +86,33 @@ public class PostsActivityTest {
         waitABit();
         waitABit();
         waitABit();
-//        onView(withId(R.id.posts_list_view)).check(matches(isDisplayed()));
+        onView(withId(R.id.posts_list_view)).check(matches(isDisplayed()));
         waitABit();
         waitABit(); // for images to load
     }
 
-//    @Test
-//    public void visibilityDisplayedAndClickable() {
-//        waitABit();
-//        waitABit();
-//        waitABit();
-//        onView(withId(R.id.posts_list_view)).check(matches(isDisplayed()));
-//        waitABit();
-//        waitABit();
-//        onView(withIndex(withId(R.id.post_visibility_icon), 0)).perform(click()); // click this button on the first post in the list
-//    }
+    @Test
+    public void visibilityDisplayedAndClickable() {
+        waitABit();
+        waitABit();
+        waitABit();
+        onView(withId(R.id.posts_list_view)).check(matches(isDisplayed()));
+        waitABit();
+        waitABit();
+        onView(withIndex(withId(R.id.post_visibility_icon), 0)).perform(click()); // click this button on the first post in the list
+    }
 
-//    @Test
-//    public void commentsButtonDisplayedAndClickable() {
-//        waitABit();
-//        waitABit();
-//        waitABit();
-//        onView(withId(R.id.posts_list_view)).check(matches(isDisplayed()));
-//        waitABit();
-//        waitABit();
-//        onView(withIndex(withId(R.id.post_comments_button), 0)).perform(click()); // click this button on the first post in the list but lol its chat-id may be null
-//        intended(hasComponent(CommentsActivity.class.getName()));
-//    }
+    @Test
+    public void commentsButtonDisplayedAndClickable() {
+        waitABit();
+        waitABit();
+        waitABit();
+        onView(withId(R.id.posts_list_view)).check(matches(isDisplayed()));
+        waitABit();
+        waitABit();
+        onView(withIndex(withId(R.id.post_comments_button), 0)).perform(click()); // click this button on the first post in the list but lol its chat-id may be null
+        intended(hasComponent(CommentsActivity.class.getName()));
+    }
 
     @Test
     public void likeButtonDisplayedAndClickable() {
