38c920981227b3dc7b65fa1f856271b06afc6de0
David Kalajdzic <david.kalajdzic@icloud.com>
2023-05-26 11:48:19 +0200

    qrcode test

--- app/src/androidTest/java/com/sdpteam/connectout/QrCode/QRcodeModalActivityTest.java
+++ app/src/androidTest/java/com/sdpteam/connectout/QrCode/QRcodeModalActivityTest.java
@@ -5,6 +5,7 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.not;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -41,6 +42,28 @@ public class QRcodeModalActivityTest {
         // check if the components are displayed
         onView(withId(R.id.modal_title)).check(matches(isDisplayed()));
         onView(withId(R.id.qrcode_image)).check(matches(isDisplayed()));
+        onView(withId(R.id.close_button)).check(matches(isDisplayed()));
+
+        // close the activity
+        activityScenario.close();
+    }
+
+    @Test
+    public void nullQrCodeData() {
+        String title = "Profile QR code";
+        String qrCodeData = "https://connect-out.com/events/0123456789";
+
+        // create the intent with extras
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), QRcodeModalActivity.class);
+        intent.putExtra("title", title);
+        // no qrCodeData string extra
+
+        // launch the activity with the intent
+        ActivityScenario<QRcodeModalActivity> activityScenario = ActivityScenario.launch(intent);
+
+        // check if the components are displayed
+        onView(withId(R.id.modal_title)).check(matches(isDisplayed()));
+        onView(withId(R.id.qrcode_image)).check(matches(not(isDisplayed())));
         onView(withId(R.id.close_button)).check(matches(isDisplayed()));
 
         // close the activity
